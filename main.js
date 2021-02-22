e868d8f5d8a529ecf3ba851940c3916c96edde9d
PikaChin <derrfgh4563@gmail.com>
2021-02-23 01:50:02 +0800

    The use of vue case 1

---
+++ main.js
@@ -0,0 +1,25 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!',
+        seen: true
+    }
+})
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+})
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        todos: [
+            {text: "fuck you js"},
+            {text: "fuck you css"},
+            {text: "fuck you html"}
+        ]
+    }
+})
